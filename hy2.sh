3bba8af6469bdbb0aa8442787dc84ceac85ba400
Kook-9527 <117556038+Kook-9527@users.noreply.github.com>
2024-10-08 09:44:37 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -14,14 +14,13 @@ read -p "请输入本地监听端口（默认为443）: " LOCAL_PORT
 LOCAL_PORT=${LOCAL_PORT:-443}
 
 read -p "请设置端口跳跃起始值（默认为60000）: " UDP_PORT_RANGE_START
-UDP_PORT_RANGE_START=${UDP_PORT_RANGE_START:-60000}
+UDP_PORT_RANGE_START=${UDP_PORT_RANGE_START:-50000}
 
 read -p "请设置端口跳跃结束值（默认为65000）: " UDP_PORT_RANGE_END
-UDP_PORT_RANGE_END=${UDP_PORT_RANGE_END:-65000}
+UDP_PORT_RANGE_END=${UDP_PORT_RANGE_END:-60000}
 
 read -p "请输入认证密码: " PASSWORD
 PASSWORD=${PASSWORD:-$(< /dev/urandom tr -dc A-Za-z0-9 | head -c20)}
-
 
 # 设置端口跳跃
 sudo iptables -t nat -A PREROUTING -i eth0 -p udp --dport $UDP_PORT_RANGE_START:$UDP_PORT_RANGE_END -j DNAT --to-destination :$LOCAL_PORT && sudo ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport $UDP_PORT_RANGE_START:$UDP_PORT_RANGE_END -j DNAT --to-destination :$LOCAL_PORT
@@ -77,3 +76,32 @@ systemctl start hysteria-server.service && systemctl enable hysteria-server.serv
 
 # 设置每天4点检测更新
 echo -e "$(crontab -l)\n0 4 * * * /bin/bash -c 'bash <(curl -fsSL https://get.hy2.sh/)' " | crontab -
+
+# --------------------
+# 节点代码生成
+# --------------------
+
+# 获取本机 IP
+local_ip=$(hostname -I | awk '{print $1}') || local_ip=$(hostname -I -6 | awk '{print $1}')
+if [ -z "$local_ip" ]; then
+  log_message "无法获取本机 IP 地址" && exit 1
+fi
+
+# 从配置文件中提取监听端口、密码和自签证书域名
+listen_port=$(grep "^listen" /etc/hysteria/config.yaml | awk '{print $2}' | cut -d ':' -f2)
+password=$(grep "password: " /etc/hysteria/config.yaml | awk '{print $2}')
+sni_domain=$(openssl x509 -noout -subject -in /etc/hysteria/server.crt | sed -n '/^subject/s/^.*CN=//p')
+
+# 检查证书域名是否为空，默认bing.com
+sni_domain=${sni_domain:-bing.com}
+
+# 生成并显示节点代码
+universal_node="hysteria2://$password@$local_ip:$listen_port/?insecure=1&sni=$sni_domain&mport=$UDP_PORT_RANGE_START-$UDP_PORT_RANGE_END#Hy2_$local_ip"
+loon_node="Hy2_$local_ip = Hysteria2,$local_ip,$listen_port,\"$password\",sni=$sni_domain,skip-cert-verify=true,download-bandwidth=250"
+surge_node="Hy2_$local_ip = Hysteria2,$local_ip,$listen_port,password=$password,port-hopping=$UDP_PORT_RANGE_START-$UDP_PORT_RANGE_END,port-hopping-interval=30,sni=$sni_domain,skip-cert-verify=true,download-bandwidth=250"
+
+echo -e "\n\033[1;32m=============================================================="
+echo -e "\n\033[1;32m通用格式节点: \n$universal_node\033[0m"
+echo -e "\n\033[1;32mLoon格式节点: \n$loon_node\033[0m"
+echo -e "\n\033[1;32mSurge格式节点: \n$surge_node\033[0m\n"
+echo -e "==============================================================\033[0m\n"
