608f2a8ca0c54ed2a55de49eaacc8bb04b82c438
Kook-9527 <117556038+Kook-9527@users.noreply.github.com>
2025-01-25 00:08:20 +0800

    更新 gost.sh

--- gost/gost.sh
+++ gost/gost.sh
@@ -102,8 +102,8 @@ function Install_ct() {
     mv gost-linux-"$bit"-"$ct_new_ver" gost
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
-    wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/qqrrooty/EZgost/main/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
-    mkdir /etc/gost && wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/qqrrooty/EZgost/main/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
+    wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/Kook-9527/VPS_Plugin/refs/heads/main/gost/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
+    mkdir /etc/gost && wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/Kook-9527/VPS_Plugin/refs/heads/main/gost/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   else
     rm -rf gost-linux-"$bit"-"$ct_new_ver".gz
     wget --no-check-certificate https://proxy.lblog.net/https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz
@@ -111,8 +111,8 @@ function Install_ct() {
     mv gost-linux-"$bit"-"$ct_new_ver" gost
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
-    wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/qqrrooty/EZgost/main/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
-    mkdir /etc/gost && wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/qqrrooty/EZgost/main/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
+    wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/Kook-9527/VPS_Plugin/refs/heads/main/gost/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
+    mkdir /etc/gost && wget --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/Kook-9527/VPS_Plugin/refs/heads/main/gost/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   fi
 
   systemctl enable gost && systemctl restart gost
@@ -916,14 +916,14 @@ function prompt_return() {
 }
 
 update_sh() {
-  ol_version=$(curl -L -s --connect-timeout 5 https://proxy.lblog.net/https://raw.githubusercontent.com/qqrrooty/EZgost/main/CN/gost.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
+  ol_version=$(curl -L -s --connect-timeout 5 https://proxy.lblog.net/https://raw.githubusercontent.com/Kook-9527/VPS_Plugin/refs/heads/main/gost/gost.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
   if [ -n "$ol_version" ]; then
     if [[ "$shell_version" != "$ol_version" ]]; then
       echo -e "存在新版本，是否更新 [Y/N]?"
       read -r update_confirm
       case $update_confirm in
       [yY][eE][sS] | [yY])
-        wget -N --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/qqrrooty/EZgost/main/CN/gost.sh
+        wget -N --no-check-certificate https://proxy.lblog.net/https://raw.githubusercontent.com/Kook-9527/VPS_Plugin/refs/heads/main/gost/gost.sh
         echo -e "更新完成"
         exit 0
         ;;
